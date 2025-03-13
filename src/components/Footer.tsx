41788740287a71f8879fb37fe4645395485912a6
Elikem <juniormedehou@gmail.com>
2025-03-13 15:35:43 +0100

    fix: footer padding

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,45 +9,45 @@ function Footer() {
     <footer className="bg-[#394E5E] text-white py-12">
       <div className="container mx-auto px-4">
         {/* Main Footer Content */}
-        <div className="grid grid-cols-1 md:grid-cols-12 gap-8 mb-8 justify-items-center">
+        <div className="flex flex-col md:flex-row justify-between items-center md:items-start mb-8">
           {/* Logo & Social Section */}
-
-          <div className="text-center md:text-left md:col-span-3 w-full">
-            <div className="flex justify-center md:justify-start w-full">
+          <div className="text-center md:text-left">
+            <div className="flex justify-center md:justify-start">
               <img src={IconLogo} alt="Zepargn Logo" className="h-16 mb-4" />
             </div>
             <p className="mb-4">{t.footer.followUs}</p>
             <div className="flex space-x-4 justify-center md:justify-start text-3xl">
-              <a href="https://x.com/ZPargn" target="_blank" className="hover:text-gray-300">
+              <a href="https://x.com/ZPargn" target="_blank" className="hover:text-gray-300 transition-colors duration-300">
                 <i className="fa-brands fa-x-twitter"></i>
               </a>
-              <a href="https://www.facebook.com/profile.php?id=61567830901375" target="_blank" className="hover:text-gray-300">
+              <a href="https://www.facebook.com/profile.php?id=61567830901375" target="_blank" className="hover:text-gray-300 transition-colors duration-300">
                 <i className="fa-brands fa-facebook"></i>
               </a>
-              <a href="https://www.instagram.com/zpargn/" target="_blank" className="hover:text-gray-300">
+              <a href="https://www.instagram.com/zpargn/" target="_blank" className="hover:text-gray-300 transition-colors duration-300">
                 <i className="fa-brands fa-instagram"></i>
               </a>
             </div>
           </div>
 
-          <div className="md:col-span-7 items-center justify-center text-center mx-auto w-full">
-            <div className="w-full">
-              <h3 className="text-xl font-semibold mb-4">
+          {/* Download App Section - Centered */}
+          <div className="w-full md:w-2/4 flex justify-center">
+            <div className="text-center">
+              <h3 className="text-xl font-semibold mb-6">
                 {t.footer.downloadApp}
               </h3>
-              <div className="flex space-x-4 justify-center">
-                <a href="https://play.google.com/store/apps/details?id=com.digitalelevate.zepargnmobileapp" target="_blank" rel="noopener noreferrer">
-                  <img src={t.hero.playStore} alt="Google Play Store" className="transform hover:scale-105 transition-transform duration-300 h-12 sm:h-14" />
+              <div className="flex items-center justify-center space-x-6">
+                <a href="https://play.google.com/store/apps/details?id=com.digitalelevate.zepargnmobileapp" target="_blank" rel="noopener noreferrer" className="transform hover:scale-105 transition-all duration-300">
+                  <img src={t.hero.playStore} alt="Google Play Store" className="h-12 sm:h-14" />
                 </a>
-                <a href="https://apps.apple.com/us/app/zepargn/id6474701827?platform=iphone" target="_blank" rel="noopener noreferrer">
-                  <img src={t.hero.appStore} alt="Apple App Store" className="transform hover:scale-105 transition-transform duration-300 h-12 sm:h-14" />
+                <a href="https://apps.apple.com/us/app/zepargn/id6474701827?platform=iphone" target="_blank" rel="noopener noreferrer" className="transform hover:scale-105 transition-all duration-300">
+                  <img src={t.hero.appStore} alt="Apple App Store" className="h-12 sm:h-14" />
                 </a>
               </div>
             </div>
           </div>
 
-
-          <div className="md:col-span-2 md:ml-0 text-center md:text-left">
+          {/* Company Links */}
+          <div className="text-center md:text-left  justify-end items-end">
             <h3 className="text-xl font-semibold mb-4">{t.footer.company}</h3>
             <ul className="space-y-2">
               <li>
@@ -75,7 +75,7 @@ function Footer() {
 
         </div>
         {/* Disclaimer Text */}
-        <div className="text-sm border-t border-white pt-8 text-white mb-8">
+        <div className="text-sm border-t border-white  pt-8 text-white mb-8">
           <p className="mb-4 text-center">
             {t.footer.disclaimer.split('politique de confidentialité').map((part: string, i: number) => {
               if (i === 0) {
